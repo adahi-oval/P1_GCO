53bbd50fb1cc976bcdd02c1437041596651d1870
alu0101333281 <65130315+alu0101333281@users.noreply.github.com>
2023-10-24 11:42:14 +0100

    funcion euclidian y mod calculateratings

--- recommender.py
+++ recommender.py
@@ -1,17 +1,5 @@
 import math
 
-# Función para calcular la distancia del coseno entre dos usuarios
-def cosineDistance(user1, user2):
-    user1Ratings, user2Ratings = commonItemArrays(user1, user2)
-
-    dotProduct = sum(user1Ratings[i] * user2Ratings[i] for i in range(len(user1Ratings)))
-    magnitudeUser1 = math.sqrt(sum(user1Ratings[i] ** 2 for i in range(len(user1Ratings))))
-    magnitudeUser2 = math.sqrt(sum(user2Ratings[i] ** 2 for i in range(len(user2Ratings))))
-
-    if magnitudeUser1 == 0 or magnitudeUser2 == 0:
-        return 0.0
-
-    return dotProduct / (magnitudeUser1 * magnitudeUser2)
 
 def readMatrix(filename): # Lee la matriz con el formato adecuado, la primera linea como valor minimo, la segunda como valor maximo y el resto de lineas como usuarios individuales
     with open(filename, "r") as matriz:
@@ -73,6 +61,55 @@ def pearsonCorelation(user1, user2): # Devuelve la correlacion de Pearson entre 
 
     return (sumNumerador / ((sumDenominador1 ** 0.5) * (sumDenominador2 ** 0.5)))
 
+# Función para calcular la distancia del coseno entre dos usuarios
+def cosineDistance(user1, user2):
+    user1Ratings, user2Ratings = commonItemArrays(user1, user2)
+
+    dotProduct = sum(user1Ratings[i] * user2Ratings[i] for i in range(len(user1Ratings)))
+    magnitudeUser1 = math.sqrt(sum(user1Ratings[i] ** 2 for i in range(len(user1Ratings))))
+    magnitudeUser2 = math.sqrt(sum(user2Ratings[i] ** 2 for i in range(len(user2Ratings))))
+
+    if magnitudeUser1 == 0 or magnitudeUser2 == 0:
+        return 0.0
+
+    return dotProduct / (magnitudeUser1 * magnitudeUser2)
+
+
+# Función para calcular la distancia euclidiana entre dos usuarios
+def euclideanDistance(user1, user2):
+    user1Ratings, user2Ratings = commonItemArrays(user1, user2)
+
+    squaredDifferences = [(user1Ratings[i] - user2Ratings[i]) ** 2 for i in range(len(user1Ratings))]
+    euclidean = math.sqrt(sum(squaredDifferences))
+
+    return euclidean
+
+
+def cosineArray(user, matrix):
+    cosineRatings = []
+
+    for otherUser in matrix:
+        if user == otherUser:
+            continue
+
+        distance = cosineDistance(user, otherUser)
+        cosineRatings.append((otherUser, distance))
+
+    return cosineRatings
+
+
+def euclideanArray(user, matrix):
+    euclideanRatings = []
+
+    for otherUser in matrix:
+        if user == otherUser:
+            continue
+
+        distance = euclideanDistance(user, otherUser)
+        euclideanRatings.append((otherUser, distance))
+
+    return euclideanRatings
+
 
 def pearsonArray(user, matrix): # Devuelve un array de tuplas de correlaciones de Pearson junto con el usuario correspondiente con el resto de usuarios de la matriz proporcionada, falta comprobacion de que el usuario esta en la matriz
 
@@ -104,51 +141,56 @@ def similarNeighbours(user, matrix, metrica, numeroVecinos):
         corrArray = sorted(pearsonArray(user, matrizSinIncompatibles), key=lambda x: x[1], reverse=True)
         for i in range(numeroVecinos):
             neighbours.append(corrArray[i])
-    elif metrica == 'cosine':  # Agregar caso para la distancia del coseno
+    elif metrica == 'cosine':  
         cosineArray = sorted(cosineArray(user, matrizSinIncompatibles), key=lambda x: x[1], reverse=True)
         for i in range(numeroVecinos):
             neighbours.append(cosineArray[i])
-
+    elif metrica == 'euclidean':
+        euclideanArray = sorted(euclideanArray(user, matrizSinIncompatibles), key=lambda x: x[1])
+        for i in range(numeroVecinos):
+            neighbours.append(euclideanArray[i])
     return neighbours
 
 
 
-def calculatePredictions(matrix, metrica, numeroVecinos, tipoPrediccion, min_val, max_val): # Funcion final que devolverá la matriz rellena con las predicciones dependiendo del método usado
-
+def calculatePredictions(matrix, metrica, numeroVecinos, tipoPrediccion, min_val, max_val):
     for user in matrix:
         if '-' not in user:
             continue
 
-        missing_indexes = [(i) for i in range(len(user)) if user[i] == '-'] # Indices de los elementos que tenemos que predecir, se usan para ver la valoracion de los demás usuarios de ese elemento
+        missing_indexes = [(i) for i in range(len(user)) if user[i] == '-']
 
-        if tipoPrediccion == 'simple': # Calcula la prediccion en base a la formula de prediccion simple
+        if tipoPrediccion == 'simple':
             sumNumerador = 0
             sumDenominador = 0
-            
-            for index in missing_indexes: # Bucle para que calcule todos los indices que faltan, porque puede haber mas de uno
-                for otherUser in similarNeighbours(user, matrix, metrica, numeroVecinos):
+
+            for index in missing_indexes:
+                neighbors = similarNeighbours(user, matrix, metrica, numeroVecinos)
+                for otherUser in neighbors:
                     sumNumerador += (otherUser[1] * float(otherUser[0][index]))
                     sumDenominador += abs(otherUser[1])
-                
-                prediction = sumNumerador / sumDenominador
-                user[index] = round(prediction, 2)
 
-        elif tipoPrediccion == 'media': # Calcula la prediccion en base a la formula de distancia con la media
+            prediction = sumNumerador / sumDenominador
+            user[index] = round(prediction, 2)
+
+        elif tipoPrediccion == 'media':
             sumNumerador = 0
             sumDenominador = 0
 
-            for index in missing_indexes: # Bucle para que calcule todos los indices que faltan, porque puede haber mas de uno
-                for otherUser in similarNeighbours(user, matrix, metrica, numeroVecinos): # Calcula para cada vecino la predicción
+            for index in missing_indexes:
+                neighbors = similarNeighbours(user, matrix, metrica, numeroVecinos)
+                for otherUser in neighbors:
                     sumNumerador += (otherUser[1] * (float(otherUser[0][index]) - userAverage(otherUser[0])))
                     sumDenominador += abs(otherUser[1])
-                
-                prediction = userAverage(user) + (sumNumerador / sumDenominador)
-                user[index] = round(prediction, 2)
+
+            prediction = userAverage(user) + (sumNumerador / sumDenominador)
+            user[index] = round(prediction, 2)
 
     return matrix
+
 
 # Main para testear, comprueben con el otro archivo de matriz2.txt también que seguro lo pedirá en clase y en la corrección
 
 ratings, min_val, max_val = readMatrix("matriz.txt")
 
-print(calculatePredictions(ratings, 'pearson', 2, 'media', min_val, max_val))
+print(calculatePredictions(ratings, 'cosine', 2, 'media', min_val, max_val))
